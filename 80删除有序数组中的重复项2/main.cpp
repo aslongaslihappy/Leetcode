b415ef1cfa364f62217a91bfd7d146ad7f1b3cd6
Tianen Li <aslongaslihappy@163.com>
2025-02-12 15:09:54 +0800

    优化

--- 80删除有序数组中的重复项2/main.cpp
+++ 80删除有序数组中的重复项2/main.cpp
@@ -4,16 +4,29 @@ using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
     int n = nums.size();
-    if (n <= 2) {
-        return n;
+    if (n == 0) {
+        return 0;
     }
-    int slow = 2, fast = 2;
+    int fast = 1, slow = 1;
     while (fast < n) {
-        if (nums[slow-2] != nums[fast]) {
+        if (nums[fast] != nums[fast - 1]) {
             nums[slow] = nums[fast];
-            slow++;
+            ++slow;
         }
-        fast++;
+        ++fast;
     }
     return slow;
+}
+
+int n;
+int mian() {
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n ; i++) {
+        cin >> nums[i];
+    }
+    int res = 0;
+    res = removeDuplicates(nums);
+    cout << res;
+    return 0;
 }
