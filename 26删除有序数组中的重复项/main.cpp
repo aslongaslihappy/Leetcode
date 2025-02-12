e0960462f3e11c880823a1d98ebdb290107a4fa4
Tianen Li <aslongaslihappy@163.com>
2025-02-12 12:39:03 +0800

    message

---
+++ 26删除有序数组中的重复项/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) {
+        return 0;
+    }
+    int fast = 1, slow = 1;
+    while (fast < n) {
+        if (nums[fast] != nums[fast - 1]) {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+        ++fast;
+    }
+    return slow;
+}
+
+int main() {
+
+    
+    return 0;
+}
